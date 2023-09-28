9ac91a011c7d3882985714ac092b4d16ac30a151
Grant Rostig <grant.github@emailmeinfo.com>
2023-09-28 14:10:50 -0500

    refactor names on singleton example

--- designp_singleton/main.cpp
+++ designp_singleton/main.cpp
@@ -1,3 +1,4 @@
+// cppmsg.com Boost 1.0 license, see: GOF 1995-SS 1.Singleton; Larman 2005-SS 26.5
 #include <bits/stdc++.h>
 using namespace std;
 struct Row {
@@ -14,30 +15,29 @@ struct Only_one_of_UDT2 {
 class Singleton {           // NOTE: In fact since I have more than one singletons in here it is more of a registry as mentioned in GOF.
 private:
     // why this bool?  well I could use std::expected, or optional, or nullptr as a flag.
-    static bool                             is_constructed;     // cannot init here.
+    static bool                             is_initialized;     // cannot init here.
     // TODO??: Enhancement request:make this into a registry of singletons.
-    static Only_one_of_UDT1                 only_one_of_udt1;   // cannot init here.
-    static Only_one_of_UDT2 const * const   only_one_of_udt2;   // cannot init here.
+    static Only_one_of_UDT1                 only_one_of_udt1_instance;   // cannot init here.
+    static Only_one_of_UDT2 const * const   only_one_of_udt2_instance;   // cannot init here.
     // TODO??: in addition let's add a shared or unique pointer to also?
 protected:
     Singleton();
 public:
-    static Only_one_of_UDT1 & access() {        // TODO??: Could this be by value instead?  I guess not since then there would be copies??  But still access() would get the right thing, but the user might not use access and get to the values some other way??
-        if ( not is_constructed ) {
+    static Only_one_of_UDT1 & instance() {        // TODO??: Could this be by value instead?  I guess not since then there would be copies??  But still access() would get the right thing, but the user might not use access and get to the values some other way??
+        if ( not is_initialized ) {
             Row row                         {51,"Fifty one."};
-            Only_one_of_UDT1 only_one_udt1  {41,"Fourty one.",row};
-            Only_one_of_UDT2 only_one_udt2  {42,"Fourty two.",row};
-            is_constructed = true;
+            Only_one_of_UDT1 only_one_udt1_instance  {41,"Fourty one.",row};
+            is_initialized = true;
         }
-        return only_one_of_udt1;
+        return only_one_of_udt1_instance;
 }};
-bool                            Singleton::is_constructed   {false};
-Only_one_of_UDT1                Singleton::only_one_of_udt1;                // TODO??: Why exactly will it not take this:{31,"Thirty one.", {32,"Thirty two."}};
-Only_one_of_UDT2 const * const  Singleton::only_one_of_udt2 {nullptr};      // TODO??: clang told me to put nullptr here.
+bool                            Singleton::is_initialized            {false};
+Only_one_of_UDT1                Singleton::only_one_of_udt1_instance;                // TODO??: Why exactly will it not take this:{31,"Thirty one.", {32,"Thirty two."}};
+Only_one_of_UDT2 const * const  Singleton::only_one_of_udt2_instance {nullptr};      // TODO??: clang told me to put nullptr here.
 //Only_one_of_UDT2 const * const  Singleton::only_one_of_udt2 {77};         // TODO??: rvalue not allowed.
 //Only_one_of_UDT2 const * const  Singleton::only_one_of_udt2 = nullptr;    // TODO??: is using = same as {}
 int main() {
-    Only_one_of_UDT1    my_singleton_udt1       = Singleton::access();
+    Only_one_of_UDT1    my_singleton_udt1       = Singleton::instance();
     int                 my_singleton_udt_int    = my_singleton_udt1.i;
     Row                 my_singleton_udt_row    = my_singleton_udt1.row;
     cout << "Have only one of this:" << my_singleton_udt1.i <<", " << my_singleton_udt1.s <<", " << my_singleton_udt1.row.ri << endl ;
