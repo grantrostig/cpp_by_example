a49ba017149652e66b24583d3cf483ba3edf97d4
Grant Rostig <grant.github@emailmeinfo.com>
2023-10-20 14:50:07 -0500

    add doc

--- designp_singleton/main.cpp
+++ designp_singleton/main.cpp
@@ -1,5 +1,5 @@
 // cppmsg.com Boost 1.0 license.
-// see: GOF 1995-SS 1.Singleton; Vlissides 1998-ch2,ch3; Larman 2005-SS 26.5; Robert Martin 2002-Singleton and Monstate; Headington 1996-SS2.7 p79; Cline 1999-ch16-?;
+// see: GOF 1995-SS 1.Singleton; Vlissides 1998-ch2,ch3; Larman 2005-SS 26.5; Robert Martin 2002-Singleton and Monstate; Headington 1996-SS2.7 p79; Cline 1999-ch16-?;https://vladris.com/blog/2017/07/10/singletons.html 
 #include "singleton_gof_with_ptr.hpp"
 #include "singleton_derived_t_ref.hpp"
 #include "singleton_gof_sample_code.hpp"
