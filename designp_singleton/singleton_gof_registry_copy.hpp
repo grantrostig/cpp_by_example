ef8c56f26cf88cfef144a63ed17a1cfd4280af01
Grant Rostig <grant.github@emailmeinfo.com>
2023-11-26 18:46:34 -0600

    just doc

--- designp_singleton/singleton_gof_registry_copy.hpp
+++ designp_singleton/singleton_gof_registry_copy.hpp
@@ -1,4 +1,5 @@
 // cppmsg.com Boost 1.0 license.
+// IGNORE, this is just a backup copy.
 // see: GOF 1995-SS 1.Singleton; Vlissides 1998-ch2,ch3; Larman 2005-SS 26.5; Robert Martin 2002-Singleton and Monstate; Headington 1996-SS2.7 p79; Cline 1999-ch16-?;
 #pragma once
 #include "../../boost-ext_ut/ut.hpp"
--- designp_singleton/singleton_grants_wish.hpp
+++ designp_singleton/singleton_grants_wish.hpp
@@ -1,9 +1,23 @@
+// Ignore for now, this thinking about a singleton registry beyond GOF's paradymn
 #pragma once
 
+//class singleton_grants_wish
 
-class singleton_grants_wish
-{
-public:
-    singleton_grants_wish();
+class Registryx {
+    // vector of singletons
 };
 
+static Registryx  registryx1 {};
+
+class Singleton1: registryx {
+    // special stuff about a topic2
+};
+
+class Singleton2: registryx2 {
+    // special stuff about a topic2
+};
+
+int test_singleton_grants_wish () {
+    Singleton1 * singleton1 {"singleton1"};
+    Singleton2 * singleton2 {"singleton2"};
+}
