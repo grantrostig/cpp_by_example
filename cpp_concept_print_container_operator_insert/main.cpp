e2fdeb42d28c2619a86aea343476f7f6fb65420a
Grant Rostig <grant.github@emailmeinfo.com>
2023-12-06 13:22:10 -0600

    reformat code

--- cpp_concept_print_container_operator_insert/main.cpp
+++ cpp_concept_print_container_operator_insert/main.cpp
@@ -13,39 +13,36 @@
 //#include <csignal>
 //#include <stacktrace>
 //#include <source_location>
-
 using std::cin; using std::cout; using std::cerr; using std::clog; using std::endl; using std::string;  // using namespace std;
 using namespace std::string_literals;
 
-
 /// Requires that a type has insertion operator
 /// Concept definition - used by a template below.
-template <typename Container>
-concept Insertable = requires( std::ostream & out ) {
-    requires not std::same_as<std::string, Container>;                                    // OR $ std::is_same <std::string, Container>::value OR std::is_same_v<std::string, Container>;
-    { out << typename Container::value_type {} } -> std::convertible_to<std::ostream & >; // OR just $ { out << typename Container::value_type {} };
+template<typename Container>
+concept Insertable = requires
+( std::ostream & out ) {
+    requires not std::same_as<std::string, Container>;  // OR $ std::is_same <std::string, Container>::value OR std::is_same_v<std::string, Container>;
+    {
+        out << typename Container::value_type {}
+    } -> std::convertible_to<std::ostream & >;          // OR just $ { out << typename Container::value_type {} };
 };
 
 /// Prints contents of a container such as a vector of int's.
 /// Concept used by Templated Function definition
-template<typename Container>                        //template<insertable Container>        // OR these 2 lines currently being used.
+template<typename Container>                            // $ template<insertable Container> OR the next 2 lines currently being used.
     requires Insertable<Container>
 std::ostream &
 operator<<( std::ostream & out, Container const & c) {
     if ( not c.empty()) {
         out << "Lenght:"<<c.size()<<",";
         out << "[";   //out.width(9);  // TODO??: neither work, only space out first element. //out << std::setw(9);  // TODO??: neither work, only space out first element.
-        //std::copy(c.begin(), c.end(), std::ostream_iterator< typename Container::value_type >( out, "," ));
-        std::copy(c.begin(), c.end(), std::ostream_iterator< typename Container::value_type >( out, "," )); // TODO??: std::prev() requires BidIrectionalIterator BiDirIt
-        //out << "]";
-        out << '\b'<<']';
-        //out << "\b]";
+        std::copy(c.begin(), c.end(), std::ostream_iterator< typename Container::value_type >( out, "," ));
         //out << "\b]"; out.width(); out << std::setw(0);
     } else out << "[CONTAINTER IS EMPTY]";
     return out;
 }
 
-///  Concept using Function Explicit instantiations that are required to generate code for linker.
+///  Concept using Function Explicit instantiations that are required to generate code for linker. TODO??: What is this again??
 ///  TODO??: is the only used if definition is in *.cpp file?
 ///  https://isocpp.org/wiki/faq/templates#templates-defn-vs-decl
 ///  https://stackoverflow.com/questions/495021/why-can-templates-only-be-implemented-in-the-header-file
@@ -56,10 +53,7 @@ operator<<( std::ostream & out, Container const & c) {
 int main ( int argc, char* arv[] ) { string my_arv { *arv}; cout << "~~~ argc,argv:"<<argc<<","<<my_arv<<"."<<endl;
     std::string                 STRING_QQQ          {"qqq"};
     std::vector<char>           QQQ                 {STRING_QQQ.begin(),STRING_QQQ.end()};
-
     cout << ":QQQ is:" << QQQ << endl;
-
-
     cout << "###" << endl;
     return EXIT_SUCCESS;
 }
