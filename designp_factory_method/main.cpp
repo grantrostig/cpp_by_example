bf44de3ec514396dd4fb1cb42cb16e28d9546580
Grant Rostig <grant.github@emailmeinfo.com>
2023-12-16 18:04:38 -0600

    clarify doc and debug text.

--- designp_factory_method/main.cpp
+++ designp_factory_method/main.cpp
@@ -72,18 +72,34 @@ operator<<( std::ostream & out, Container const & c) {
 } */
 // ************************** Beginning of Example **************************************************************
 
-enum class Kind {
+
+/* ## Bing "Definition of token"
+In computer programming, a token is the smallest individual element that is meaningful to the compiler. It's a single element of a programming
+language. Tokens are the building blocks of a programming language. There are five categories of tokens:
+
+- Keywords: These are pre-defined or reserved words in a programming language. Each keyword is meant to perform a specific function in a program.
+- Identifiers: These are user-defined names used for naming variables, functions, and arrays.
+- Constants: These refer to the variables with fixed values.
+- Strings: These are arrays of characters ended with a null character (\\0).
+- Operators: These are symbols that perform operations on one or more operands.
+
+Tokens are used in many different contexts in programming. For example, in an authentication system, a token can represent the fact that a user
+is authenticated. In a programming language like C, tokens can be keywords, identifiers, constants, strings, and special symbols.
+*/
+
+enum class Kind {           // Probably entirely misunderstood.  Was thinking of parsing tokens. See above "Definition of token".
     not_set,
     my_kind_of_token_1,
     my_kind_of_token_2
 };
 
 class Base {
-protected:  class Token {
+protected:  class Token {  // Probably entirely misunderstood reason for this. Was thinking of parsing tokens. May just be a "marker" or name "handle" for use in make_unique to specify a type?  See above "Definition of token".
             public: Token() {
-                    cerr << ":constructor of Base::Token() started running."<<endl; }
-                    Kind            token_kind_as_an_enum{Kind::not_set};
-                    std::string     token_value_as_a_string{"base::Token string data member inited"};
+                    // TODO??: Probably nothing, based on the source example, but what if anything, is a good example of what should be done here?
+                    cerr << ":constructor of Base::Token() is/has run."<<endl; }
+                    //Kind            token_kind_as_an_enum{Kind::not_set};
+                    //std::string     token_value_as_a_string{"base::Token string data member inited"};
             };
 
             virtual void post_initialize() {            // Called right after construction
@@ -95,9 +111,10 @@ protected:  class Token {
             }
 public:     explicit Base( Token my_token ) {           // Create an imperfectly initialized object
                 cerr << ":constructor of Base( Token ) has started running."<<endl;
-                if (  my_token.token_kind_as_an_enum == Kind::my_kind_of_token_1 ) {
-                    /* ... */  // TODO??: What is a good example of what should be done here?
-                };
+                /* ... */  // TODO??: What if anything is a good example of what should be done here? Nothing except ... shown in source example.
+                /* if (  my_token.token_kind_as_an_enum == Kind::my_kind_of_token_1 ) {
+                    cerr << ":If within::constructor of Base( Token ) has started running."<<endl;
+                }; */
                 my_base_int = 11;  ++my_base_int; my_base_string = "base data member inited in constructor";  // TODO??: Is this a good example of what should be done here?
                 cerr << ":constructor of Base( Token ) has run."<<endl;
             }
@@ -105,7 +122,7 @@ public:     explicit Base( Token my_token ) {           // Create an imperfectly
 
             template<class T> static std::unique_ptr<T> create_u() {        // Interface for creating unique objects
                 cerr << ":Base::create_u() starting run."<<endl;
-                auto p = std::make_unique<T>( typename T::Token{} );
+                auto p = std::make_unique<T>( typename T::Token{} );// TODO??: What exactly does this do here: $ typename T::Token{} // specifically  Token{}
                 p->post_initialize();
                 cerr << ":Base::create_u() has run."<<endl;
                 return p;
@@ -122,26 +139,25 @@ public:     explicit Base( Token my_token ) {           // Create an imperfectly
             int my_base_fn() { return 15; }
 };
 
-class Derived_1 : public Base {                           // User derived class
-protected:  class Token {
+class Derived_1 : public Base {    // API User derived class
+protected:  class Token {          // TODO??: This hides Base::Token ! Was that intentional in source example?  Probably...some template magic?
                 public: Token() {
-                    cerr << ":Constructor of Derived_1::Token() started running."<<endl;
                     // TODO??: Probably nothing based on the source example, but what if anything, is a good example of what should be done here?
-                    cerr << ":Constructor of Derived_1::Token() has run."<<endl;
+                    cerr << ":Constructor of Derived_1::Token() started running."<<endl;
                 }
-                Kind            token_kind_as_an_enum{Kind::not_set};
-                std::string     token_value_as_a_string{"Derived_1::Token string data member inited"};
+                //Kind            token_kind_as_an_enum{Kind::not_set};
+                //std::string     token_value_as_a_string{"Derived_1::Token string data member inited"};
             };
-            template<class T> friend std::unique_ptr<T> Base::create_u();
-            template<class T> friend std::shared_ptr<T> Base::create_s();
-public:     explicit Derived_1( Token ) : Base { Base::Token{} } {
+protected:          template<class T> friend std::unique_ptr<T> Base::create_u();
+protected:          template<class T> friend std::shared_ptr<T> Base::create_s();
+public:     explicit Derived_1( Token ) : Base { Base::Token{} } {  // TODO??: Is this :Base{} constructing the base classes' data structures? OR is it initializing member variables (with constants C.45)
                 /* ... */  // TODO??: What is a good example of what should be done here?
-                    cerr << ":constructor of Derived_1( Token ) has run."<<endl;
+                    cerr << ":constructor of Derived_1( Token ) is/has run."<<endl;
             }
             void setup_object_before_use_Zcalled_withinZ_post_initialize() override final {  // Called f() in CppCoreGuidelines C.50
                 /* ... */  // TODO??: What is a good example of what should be done here?
                 my_derived_1_int = 101;  ++my_derived_1_int; my_derived_1_string = "derived_1 data member inited in constructor";
-                cerr << ":Derived_1::setup_object_before_use_Zcalled_withinZ_post_initialize() has run."<<endl;  // TODO??: Is this a good example of what should be done here?
+                cerr << ":Derived_1::setup_object_before_use_Zcalled_withinZ_post_initialize() is/has run."<<endl;  // TODO??: Is this a good example of what should be done here?
             };
             // *****  We consider this, below to be the functionality API of the class?  Not in the source example.
             int my_derived_1_int{100};
@@ -184,13 +200,13 @@ int main (int argc, char* argv[]) { string my_argv {*argv};cerr<< "~~~ argc,argv
     cout << uniq_ptr_1.get()->my_derived_fn()    << endl;;
 
     /* std::unique_ptr<Derived_2> uniq_ptr_2 { Derived_2::create_u<Derived_2>() };
-    cout << ":*** Unique ptr 2 ***" << endl;;
+    cout << ":*** Unique ptr Derived_2 ***" << endl;;
     cout << uniq_ptr_2.get()->my_base_int       << endl;;
     cout << uniq_ptr_2.get()->my_derived_2_int  << endl;;
     cout << uniq_ptr_2.get()->my_derived_fn()   << endl;; */
 
-    std::shared_ptr<Derived_1> shared_ptr_1 = Derived_1::create_u<Derived_1>();  // creating a Derived object
-    cout << ":*** Shared ptr ***"    << endl;;
+    std::shared_ptr<Derived_1> shared_ptr_1 = Derived_1::create_s<Derived_1>();  // creating a Derived object
+    cout << ":*** Shared ptr Derived_1 ***"    << endl;;
     cout << shared_ptr_1.get()->my_base_int     << endl;;
     cout << shared_ptr_1.get()->my_derived_1_int<< endl;;
     cout << shared_ptr_1.get()->my_derived_fn() << endl;;
