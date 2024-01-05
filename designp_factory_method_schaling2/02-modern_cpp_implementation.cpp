a53d620e1a3f61be834dc2a8426c4c1cfafdaba9
Grant Rostig <grant.github@emailmeinfo.com>
2024-01-05 12:37:56 -0600

    bug unique_ptr.reset(new DifferentType)

--- designp_factory_method_schaling2/02-modern_cpp_implementation.cpp
+++ designp_factory_method_schaling2/02-modern_cpp_implementation.cpp
@@ -1,7 +1,6 @@
 #include <cassert>
 #include <iostream>
 #include <memory>
-#include <utility>
 using namespace std;
 namespace test_02_ns {
 struct I_Connection {                               // Abstract class or Interface
@@ -32,11 +31,11 @@ struct UDPConnectionFactory final : public I_ConnectionFactory {
 
 #define PASS_BY_REF
 #ifdef PASS_BY_REF
-  void use_factory_hello(I_ConnectionFactory &i_conFactory) {
+  void use_factory_hello(           I_ConnectionFactory &i_conFactory) {
     unique_ptr<I_Connection> i_con = i_conFactory.make();  // static type
     i_con->send("Hello");                       // dynamic type is run
   }
-  void use_factory_bye(                I_ConnectionFactory & i_conFactory) {
+  void use_factory_bye(             I_ConnectionFactory & i_conFactory) {
     unique_ptr<I_Connection> i_con = i_conFactory.make();  // static type
     i_con->send("Bye");                       					// dynamic type is run
   }
@@ -57,37 +56,44 @@ auto use_factory_return_it(unique_ptr<I_ConnectionFactory> i_conFactory) {
     return i_conFactory;
 }}
 
-void test_02() {  using namespace test_02_ns;
+void test_02() {  using namespace test_02_ns; cout << "Start test 02" << endl;
     unique_ptr<I_ConnectionFactory> i_conFactory_uptr{};   		// static type, but NULL
     i_conFactory_uptr = make_unique<TCPConnectionFactory>();   	// static type, loaded.
                         // OR JUST: unique_ptr<I_ConnectionFactory> i_conFactory_uptr{make_unique<TCPConnectionFactory>()};
+    //i_conFactory_uptr.reset( make_unique<TCPConnectionFactory>() );   // TODO??: Why? FAIL static type, loaded.
+    i_conFactory_uptr.reset( new TCPConnectionFactory );              // static type, loaded
+    //i_conFactory_uptr.reset( new UDPConnectionFactory );              // TODO??:
 
-    //i_conFactory_uptr.reset( make_unique<TCPConnectionFactory>() );// TODO??: Why? FAIL static type, loaded.
-    i_conFactory_uptr.reset(new TCPConnectionFactory);                  // static type, loaded
-
-    #ifdef PASS_BY_REF
+#ifdef PASS_BY_REF
     use_factory_hello(*i_conFactory_uptr);	// We didn't pass the pointer, or ownership, just the value, which is a covariant factory class
-    use_factory_bye(*i_conFactory_uptr);
-    #else
-    //use_factory_hello(move(i_conFactory_uptr));
+    use_factory_bye(  *i_conFactory_uptr);
+#else
+                                            //use_factory_hello(move(i_conFactory_uptr));                       // BAD: We loose posession TODO??: permanently?
     i_conFactory_uptr = use_factory_return_it(move(i_conFactory_uptr));
                                 // i_conFactory_uptr = use_factory_return_it(i_conFactory_uptr);			// FAILS because we can't copy it.
     assert(i_conFactory_uptr.get());
     use_factory_bye(move(i_conFactory_uptr));
-    #endif
+#endif
 
-    unique_ptr<I_ConnectionFactory> i_conFactory_uptr_md{make_unique<TCPConnectionFactory>()};   // static type
-    #ifdef PASS_BY_REF
-    use_factory_hello(*i_conFactory_uptr_md);
-    #else
-    use_factory_hello(move(i_conFactory_uptr_md));
-    #endif
+    unique_ptr<I_ConnectionFactory> i_conFactory_uptr_maked{make_unique<TCPConnectionFactory>()};   // Yes obviously, but still checking it. :)
+#ifdef PASS_BY_REF
+    use_factory_hello(*i_conFactory_uptr_maked);
+#else
+    use_factory_hello(move(i_conFactory_uptr_maked));
+#endif
 
-    i_conFactory_uptr.reset(nullptr);
+    // *** UDP is second type ***
+    //i_conFactory_uptr.reset(nullptr);
     i_conFactory_uptr.reset(new UDPConnectionFactory);                                                  // static type
+#ifdef PASS_BY_REF
+    use_factory_hello( *i_conFactory_uptr);
+#else
     use_factory_hello(move(i_conFactory_uptr));
-    use_factory_hello(i_conFactory_uptr);
+#endif
+
     i_conFactory_uptr.reset(nullptr);
+    i_conFactory_uptr_maked.reset(nullptr);
+    cout << "End test 02"<< endl;
 }
 
 
