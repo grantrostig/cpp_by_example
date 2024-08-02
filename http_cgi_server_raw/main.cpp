3fb10ed251f8c2a1cb46e84d75cd587a31a4021a
Grant Rostig <grant.github@emailmeinfo.com>
2024-08-02 01:09:22 -0500

    refactor,runs.

--- http_cgi_server_raw/main.cpp
+++ http_cgi_server_raw/main.cpp
@@ -49,11 +49,12 @@ Browse to: http://your_vps_ip/cgi-bin/hello.cgi
 # sudo systemctl restart apache2
 */
 
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
-int
-main() {
+void
+write_webpage_init() {
     cout << "Content-type: text/html\r\n\r\n"
          << "<!DOCTYPE html>" //
          << "<html lang=\"en\">"
@@ -64,29 +65,45 @@ main() {
          << "<body>"
          << "<h1>Hello from C++ CGI!</h1>"
          << "Enter info below:";
+}
 
-    char *query_string = getenv("QUERY_STRING");
-    std::string name;
+void
+write_webpage_finalize() {
+    cout << "</body></html>" << endl;
+}
+
+std::string
+get_page_parameter(std::string parameter_name) {
+    const char *query_string = getenv("QUERY_STRING");
     if(query_string != nullptr) {
-        std::string qs{query_string};
-        size_t      itr{qs.find("name=")};
-        if(itr != std::string::npos) { name = qs.substr(itr + 5); }
+        std::string const qs{ query_string };
+        size_t const      itr{ qs.find(parameter_name + "=") };
+        if(itr != std::string::npos) { return qs.substr(itr + 5); }
     }
+    return "";
+}
+
+void
+get_user_form_input(std::string name) {
     cout << "<form method='get'>"
          << "   <label for= 'nameid'>Name (44 or more characters):</label>"
          << "   <input type= 'text' id= 'nameid' name= 'name' "
          << "       value= '" << name << "'"
          << "       required minlength= '44' maxlength= '999' size= '44' />"
-
          << "   <input type= 'submit' value= 'Submit'>"
          << "</form>" << endl;
-
     if(!name.empty()) {
         cout << "<p>$$ Hello, " << name << "!</p>";
     } else {
         cout << "<p>$$ Got no input from you.</p>";
     }
+}
 
-    cout << "</body></html>" << endl;
+int
+main() {
+    write_webpage_init();
+    std::string const name{ get_page_parameter("name") };
+    get_user_form_input(name);
+    write_webpage_finalize();
     return 0;
 }
