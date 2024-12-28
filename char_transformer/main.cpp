eafb93dcbdd4afeee19370a513d8c3a98709698a
Grant Rostig <grant.github@emailmeinfo.com>
2024-12-28 14:09:20 -0600

    clarified purpose,runs.

--- char_transformer/main.cpp
+++ char_transformer/main.cpp
@@ -1,5 +1,5 @@
-// code by Jon Kalb https://wandbox.org/permlink/R5tMMsppwL5oSTpy
-// edited by Grant Rostig
+// Code by Jon Kalb https://wandbox.org/permlink/R5tMMsppwL5oSTpy
+// Edited by Grant Rostig
 // boost 1.0 license
 #include <algorithm>
 #include <iterator>
@@ -10,55 +10,33 @@
 #include <vector>
 #include <unordered_map>
 
-/* std::unordered_map<char, int> transform_values
-    {
-        {'T', 3},
-        {'h', 3},
-        {'i', 3},
-        {'s', 4},
-        {' ', 1},
-        {'e', 1},
-        {'x', 2},
-        {'t', 3},
-        {'.', 1}
-}; */
-
-std::map<char, std::tuple<int, std::vector<int>>> transform_map
-    {
-        {'T', {0, {1, 2, 3}}},
-        {'h', {0, {4, 5, 6}}},
-        {'i', {0, {7, 8}}},
-        {'s', {0, {9, 10, 11, 12}}},
+std::map<char, std::tuple<int, std::vector<int>>> transform_map {
+   //   char, counter, replacement values
+        {'a', {0, {1, 2, 3}}},
+        {'b', {0, {4, 5, 6}}},
+        {'c', {0, {7, 8}}},
+        {'d', {0, {9, 10, 11, 12}}},
         {' ', {0, {13}}},
-        {'e', {0, {15}}},
-        {'x', {0, {16, 17}}},
-        {'t', {0, {18, 19, 20}}},
-        {'.', {0, {13}}}
+        {'.', {0, {1,14}}}
 };
 
-auto main() -> int
-{
-    std::string password{"This is text."};
-    std::vector<int> output(size(password));
-
-    auto transformer{[tmap{transform_map}] (char c) mutable
-                      {
-                          auto& counter{get<0>(tmap[c])};
-                          auto& values{get<1>(tmap[c])};
-                          auto result{values[counter]};
+auto main() -> int {
+    std::string         text{" abcd. abcd."};
+    std::vector<int>    output(size(text));
+    auto transformer{ [tmap{transform_map}] (char c) mutable { // replace char with one of a vector of numbers, incrementing the index to find replacement value.
+                          auto& counter{get<0>(tmap[c])};  // TODO??: making this a ref somehow preserves the value similar to "static"?
+                          auto  values{get<1>(tmap[c])};
+                          auto  result{values[counter]};
                           ++counter;
-                          //if (counter == ssize(values))  // TODO??: why did Jon type ss?
-                          if (counter == size(values))
-                          {
+                          if (counter == ssize(values)) { // signed size // Rap around when we have used all of them?
                               counter = 0;
                           }
                           return result;
                       }};
-
-    std::transform(begin(password), end(password), begin(output), transformer);
-    std::cout << "test\n";
-    for (int v: output)
-    {
-        std::cout << v << '\n';
-    }
+    std::transform(begin(text), end(text), begin(output), transformer);
+    std::cout << "size(password):" <<     size(text) << std::endl;
+    std::cout << "size(transform_map)" << size(transform_map) << std::endl;
+    std::cout << "size(output)" << size(output) << std::endl;
+    for (int v: output) { std::cout << v << ','; }
+    std::cout << std::endl;
 }
