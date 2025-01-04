81c533b87fa59c1f5bde964b748b2498609f541e
Grant Rostig <grant.github@emailmeinfo.com>
2025-01-04 00:21:47 -0600

    runs, debugged and fixed output format.

--- cpp_concept_print_container_operator_insert/main.cpp
+++ cpp_concept_print_container_operator_insert/main.cpp
@@ -13,18 +13,11 @@
 #include <chrono>
 #include <csignal>
 #include <iostream>
+#include <iterator>
 #include <map>
 #include <source_location>
 #include <stacktrace>
 #include <utility>
-
-using std::cin; using std::cout; using std::cerr; using std::clog; using std::endl; using std::string;  // using namespace std;
-template<typename First, typename Second, typename Third>
-std::ostream & operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple);
-template<typename First, typename Second>
-std::ostream & operator<<( std::ostream & out, std::pair<First,Second> const & my_pair);
-
-#include <iterator>
 
 using std::cin; using std::cout; using std::cerr; using std::clog; using std::endl; using std::string;  // using namespace std;
 using namespace std::string_literals;
@@ -55,10 +48,19 @@ inline void PRINT_ELEMENTS (const T& coll, string optcstr="") {
 #define LOGGERX(  msg, x)using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<    "["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<".:{"<<x<<"}."    <<endl;cout.flush();cerr.flush();
 #define LOGGERXR( msg, x)using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<"\r\n["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<".:{"<<x<<"}.\r\n"<<endl;cout.flush();cerr.flush();
 
-template<typename First, typename Second, typename Third>
+template<typename First, typename Second, typename Third>  // this forward declaration is definitely required to compile
 std::ostream & operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple);
-template<typename First, typename Second>
-std::ostream & operator<<( std::ostream & out, std::pair<First,Second> const & my_pair);
+
+template<class First, class Second>
+std::ostream &
+operator<<( std::ostream & out, std::pair<First,Second> const & my_pair) {
+    out << "PAIR_MEMBERS["; out << my_pair.first  <<","<< my_pair.second; out << "]"; return out; }
+
+template<class First, class Second, class Third>  // TODO??: make it variadic. TODO??: what about something like this: std::copy(my_pair.begin(), my_pair.end(), std::ostream_iterator< typename Container::value_type >( out, ">,<" ));
+std::ostream &
+operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple) {
+ out << "TUPLE_MEMBERS["; out << std::get<0>(my_tuple) << "," << std::get<1>(my_tuple) << "," << std::get<2>(my_tuple); out << "]"; return out; }  //out << "\b\b\b>]"; out.width(); out << std::setw(0);
+
 
 template <class T>
 concept Streamable
@@ -78,55 +80,53 @@ concept Streamable_container
 template<Streamable_container SC>    // Function Template with typename concept being restricted to SC in this case. Similar to Value template
 std::ostream &
 operator<<( std::ostream & out, SC const & sc) {
-    LOGGER_( "Streamable_container<>():")
+    LOGGER_()
     if ( not sc.empty() ) {
-        out << "[<";                    //out.width(9);  // TODO??: neither work, only space out first element. //out << std::setw(9);  // TODO??: neither work, only space out first element.
-        //std::copy(sc.begin(), sc.end(), std::ostream_iterator< typename SC::value_type >( out, ">,<" ));
-        for (auto &i:sc) {
-            out << i << ">,<";
+        out << "[";                    //out.width(9);  // TODO??: neither work, only space out first element. //out << std::setw(9);  // TODO??: neither work, only space out first element.
+        // TODO??: why compile error on?: std::copy(sc.begin(), sc.end(), std::ostream_iterator< typename SC::value_type >( out, ">,<" ));
+        bool first{ true };
+        for(auto &i : sc) { // this works, but why not std::copy?
+            if(first) {
+                first = false;
+                out << "<" << i;
+            } else {
+                out << ">,<" << i;
+            }
         }
         out << ">]" << " ";             // out.width(); out << std::setw(0);
     } else
         out << "[CONTAINTER IS EMPTY]";
+
     return out;
 }
 
-template<class First, class Second>
-std::ostream &
-operator<<( std::ostream & out, std::pair<First,Second> const & my_pair) {
-    out << "PAIR_MEMBERS["; out << my_pair.first  <<","<< my_pair.second; out << "]"; return out; }
-
-template<class First, class Second, class Third>  // TODO??: make it variadic. TODO??: what about something like this: std::copy(my_pair.begin(), my_pair.end(), std::ostream_iterator< typename Container::value_type >( out, ">,<" ));
-std::ostream &
-operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple) {
-    out << "TUPLE_MEMBERS["; out << std::get<0>(my_tuple) << "," << std::get<1>(my_tuple) << "," << std::get<2>(my_tuple); out << "]"; return out; }  //out << "\b\b\b>]"; out.width(); out << std::setw(0);
 
 int main ( int argc, char* arv[] ) { string my_arv { *arv}; cout << "~~~ argc,argv:"<<argc<<","<<my_arv<<"."<<endl;
     using My_tuple = std::tuple<int,float,std::string>;
     std::pair<int,float>                my_fundamental_pair {2, 3.3f};
     My_tuple                            my_tuple            {2, 3.3f, "threethreethree"s};
+    std::map<int, My_tuple>::value_type my_value_type       {};
     std::pair                           my_tuple_pair       {my_tuple,my_tuple};
     std::vector                         v_int               {1,2,3};
     std::vector                         v_cstring           {"one","two","three"};
     std::vector                         v_string            {"one"s,"two"s,"three"s};
-    std::vector                         v_fpair             {my_fundamental_pair,my_fundamental_pair,my_fundamental_pair};
+    std::vector                         v_fund_pair         {my_fundamental_pair,my_fundamental_pair,my_fundamental_pair};
     std::vector                         v_tuple             {my_tuple,my_tuple,my_tuple};
-    std::map<int, My_tuple>::value_type m_v_type            {};
-    std::vector                         v_value_type        {m_v_type, m_v_type, m_v_type};
+    std::vector                         v_value_type        {my_value_type, my_value_type, my_value_type};
     std::map<int, My_tuple>             my_map              { {10,my_tuple} };
-    std::map<int, My_tuple>             my_map2             { { 10,{11,12.0f,"1string"s}}, {20,{21,22.0f,"2string"s}} };
+    std::map<int, My_tuple>             my_map2             { { 10,{11,12.0f,"1st string"s}}, {20,{21,22.0f,"2nd string"s}} };
 
+    cout << "$$My value_type      :"<< my_value_type << endl;
+    cout << "$$My v_value_type    :"<< v_value_type << endl;
     cout << "$$My fundamental pair:"<< my_fundamental_pair << endl;        //cout << my_pair.first <<", " << my_pair.second << endl;
-    cout << "$$My tuple:"           << my_tuple << endl;                             //cout << std::get<0>(my_tuple) <<"," << std::get<1>(my_tuple) << "," << std::get<2>(my_tuple) << endl;
-    cout << "$$My tuple pair:"      << my_tuple_pair << endl;
-    cout << "$$My v_int"            << v_int << endl;
-    cout << "$$My v_cstring"        << v_cstring << endl;
-    cout << "$$My v_string"         << v_string << endl;
-    cout << "$$My v_fpair:"         << v_fpair << endl;  // ERROR
-    cout << "$$My map:"             << my_map << endl;
-    cout << "$$My map2:"            << my_map2 << endl;
-    cout << "$$My_value_type:"      << m_v_type << endl;
-    cout << "$$My v_value_type:"    << v_value_type << endl;
+    cout << "$$My tuple           :"<< my_tuple << endl;                             //cout << std::get<0>(my_tuple) <<"," << std::get<1>(my_tuple) << "," << std::get<2>(my_tuple) << endl;
+    cout << "$$My tuple pair      :"<< my_tuple_pair << endl;
+    cout << "$$My v_int           :"<< v_int << endl;
+    cout << "$$My v_cstring       :"<< v_cstring << endl;
+    cout << "$$My v_string        :"<< v_string << endl;
+    cout << "$$My v_fund_pair     :"<< v_fund_pair << endl;  // ERROR
+    cout << "$$My map2            :"<< my_map2 << endl;
+    cout << "$$My map             :"<< my_map << endl;
 
     cout << "###" << endl;
     return EXIT_SUCCESS;
