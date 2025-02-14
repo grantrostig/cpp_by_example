55e7c19d2455a609a60c18ffe6053aa0afd75030
Grant Rostig <grant.github@emailmeinfo.com>
2025-02-14 17:27:24 -0600

    new project

---
+++ linkage_linking_bindage/global_entities.hpp
@@ -0,0 +1,162 @@
+/** Copyright (c) Grant Rostig, grantrostig.com 2023. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+    NOT PRODUCTION QUALITY CODE, just shows learning/teaching example, not real programming, don't copy this style, just playing around
+    Reminder of usefull resources:
+        https://coliru.stacked-crooked.com/
+        https://godbolt.org/
+        https://cppinsights.io/
+        https://Wandbox.org
+        https://cpp.sh/
+        https://quick-bench.com/
+        https://arnemertz.github.io/online-compilers/
+    [[maybe_unused]] int * my_new_var8 = ::new (22,int);
+    Language Versions:  clan++ -std=c++2b, g++ -std=c++23 or =gnu++23
+    STD Libraries: Gcc: libstdc++; Clang: libc++; Microsoft: msvc/ms stl  // TODO??: how do we link with different libraries than the default using gcc or clang (the exact command line text)?
+    g++ -Werror -Weffc++ -Wextra -Wall -Wconversion -Wshadow -Wpedantic -Wold-style-cast -Wsign-promo -Wzero-as-null-pointer-constant -Wsuggest-override -Wnon-virtual-dtor \
+        -Wcast-align -Woverloaded-virtual -Wunused -pedantic -Wsign-conversion -Wmisleading-indentation -Wnull-dereference -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough \
+        -Wuseless-cast -Wsuggest-final-types -Wsuggest-final-methods -Wduplicated-cond -Wduplicated-branches -Wlogical-op -std=gnuc++23 \
+        main.cpp <OTHER>.cpp -o <A.OUT>
+
+    SYMBOL    MEANING // for debugging purposes
+    TODO:     the principal programmer needs todo.
+    TODO?:	  the principal programmer is not sure about something, that should be addressed.
+    TODO?:X   the X programmer is not sure about something, that should be addressed.
+    TODO??:   is a question for verbal discussion at CppMSG.com meetup meetings.
+
+    Ordering of headers as follows: re:Lakos
+    + The prototype/interface header for this implementation (ie, the .h/.hh file that corresponds to this .cpp/.cc file).
+    + Other headers from the same project, as needed.
+    + Headers from other non-standard, non-system libraries (for example, Qt, Eigen, etc).
+    + Headers from other "almost-standard" libraries (for example, Boost)
+    + Standard C++ headers (for example, iostream, functional, etc.)
+    + Standard C headers (for example, cstdint, dirent.h, etc.)
+
+    define NDEBUG if asserts are NOT to be checked.  Put in *.h file not *.CPP
+    #define NDEBUG
+    define GR_DEBUG if we/programmer is Debugging.  Put in *.h file not *.CPP
+    #define GR_DEBUG
+    #ifdef GR_DEBUG
+    #endif GR_DEBUG
+ */
+#pragma once
+//#include <boost/dynamic_bitset.hpp>
+//#include <boost/multiprecision/cpp_int.hpp>
+//#include <dlib/numeric_constants.h>
+//#include <bits/stdc++.h>
+//#include <gsl/gsl>      // sudo dnf install  guidelines-support-library-devel
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <csignal>
+#include <iostream>
+#include <iterator>
+#include <stacktrace>
+#include <optional>
+#include <source_location>
+#include <string>
+#include <stacktrace>
+#include <vector>
+
+using std::cin; using std::cout; using std::cerr; using std::clog; using std::endl; using std::string;  // using namespace std;
+using namespace std::string_literals;
+using namespace std::chrono_literals;
+
+// SYMBOL    MEANING // for debugging purposes
+// TODO:    the principal programmer needs todo.
+// TODO?:	the principal programmer is not sure about something, that should be addressed.
+// TODO?:X  the X programmer is not sure about something, that should be addressed.
+// TODO??:  is a question for verbal discussion at CppMSG.com meetup meetings.
+
+//static_assert(std::endian::native != std::endian::little && std::endian::native != std::endian::big , "Memory is mixed endian.");
+static_assert(std::endian::native == std::endian::little, "Memory is big endian or mixed.");  // May or may not be relevant or required.
+#pragma message("$$ Memory is little endian not mixed, not big.")
+//static_assert(std::endian::native == std::endian::big,  "Memory is little endian or mixed.");
+
+// define   NDEBUG if asserts are NOT to be checked.  Put in *.h file not *.CPP
+//#define 	NDEBUG
+// define   GR_DEBUG if we/programmer is Debugging.  Put in *.h file not *.CPP
+//#define  	GR_DEBUG
+//#ifdef   	GR_DEBUG
+//#endif    GR_DEBUG
+
+static_assert(CHAR_MIN < 0, "Char is signed.");
+//static_assert(CHAR_MIN == 0, "Char is unsigned.");
+#if CHAR_MIN < 0
+#pragma message("$$ Char is signed.")
+#else
+#pragma message("$$ Char is unsigned.")
+#endif
+
+#pragma message("$$ Twos Complement integer math most common, and C++ standard required since C++20.")
+
+using Ostring       = std::optional<std::string>;
+using Ochar         = std::optional<char>;
+using Ointegral     = std::optional<long>;
+
+//inline constexpr char           CHAR_NULL{'\o{177}'};        // Value is unset/not-set, similar to how a SQL DB shows an unset field as NULL, which is different than zero length or some magic number.  Here we turn it into a magic number and hope for the best.
+inline constexpr char           CHAR_NULL{CHAR_MIN};        // Value is unset/not-set, similar to how a SQL DB shows an unset field as NULL, which is different than zero length or some magic number.  Here we turn it into a magic number and hope for the best.
+inline constexpr signed char    SCHAR_NULL{SCHAR_MIN};       // Value is unset/not-set, similar to how a SQL DB shows an unset field as NULL, which is different than zero length or some magic number.  Here we turn it into a magic number and hope for the best.
+inline constexpr unsigned char  UCHAR_NULL{UCHAR_MAX};       // Value is unset/not-set, similar to how a SQL DB shows an unset field as NULL, which is different than zero length or some magic number.  Here we turn it into a magic number and hope for the best.
+inline constexpr std::string    STRING_NULL{"NULL"};    // Value is unset/not-set, similar to how a SQL DB shows an unset field as NULL, which is different than zero length or some magic number.  Here we turn it into a magic number and hope for the best.
+
+ // Some crude logging that prints source location, where X prints a variable, and R adds \n\r (which is usefull when tty in in RAW or CBREAK mode. Requires C++20.
+#define LOGGER_(  msg )  using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<    "["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<           "."    <<endl;cout.flush();cerr.flush();
+#define LOGGER_R( msg )  using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<"\r\n["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<           ".\r\n"<<endl;cout.flush();cerr.flush();
+#define LOGGERX(  msg, x)using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<    "["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<".:{"<<x<<"}."    <<endl;cout.flush();cerr.flush();
+#define LOGGERXR( msg, x)using loc = std::source_location;std::cout.flush();std::cerr.flush();std::cerr<<"\r\n["<<loc::current().file_name()<<':'<<std::setw(4)<<loc::current().line()<<','<<std::setw(3)<<loc::current().column()<<"]`"<<loc::current().function_name()<<"`:" <<#msg<<".:{"<<x<<"}.\r\n"<<endl;cout.flush();cerr.flush();
+
+template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+
+// **** Version 0.6 of project: cpp_concept_print_container_operator_insert
+template<typename First, typename Second, typename Third>  // this forward declaration is definitely required to compile
+std::ostream & operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple);
+
+template<class First, class Second>  // Must precede Streamable
+std::ostream &
+operator<<( std::ostream & out, std::pair<First,Second> const & my_pair) {
+    out << "PAIR_MEMBERS["; out << my_pair.first  <<","<< my_pair.second; out << "]"; return out; }
+
+template<class First, class Second, class Third>   // Must precede Streamable
+                                                  // TODO??: make it variadic. TODO??: what about something like this: std::copy(my_pair.begin(), my_pair.end(), std::ostream_iterator< typename Container::value_type >( out, ">,<" ));
+std::ostream &
+operator<<( std::ostream & out, std::tuple<First,Second,Third> const & my_tuple) {
+    out << "TUPLE_MEMBERS["; out << std::get<0>(my_tuple) << "," << std::get<1>(my_tuple) << "," << std::get<2>(my_tuple); out << "]"; return out; }  //out << "\b\b\b>]"; out.width(); out << std::setw(0);
+
+template <class T>
+concept Streamable
+    = requires( std::ostream & out_concept_parameter ) {
+          { out_concept_parameter << T {} } -> std::convertible_to<std::ostream &>;   // bool concept_function definition()
+      };
+
+template <class Container>
+concept Streamable_container
+    = requires( std::ostream & out ) {
+          requires not std::same_as<std::string, Container>;                          // bool concept_requires requirement()
+          requires not std::same_as<std::string_view, Container>;
+          requires     Streamable<typename Container::value_type>;
+          // OLD WORKING STUFF { out << typename Container::value_type {} } -> std::convertible_to<std::ostream & >; // OR just $ { out << typename Container::value_type {} };
+      };
+
+template<Streamable_container SC>    // Function Template with typename concept being restricted to SC in this case. Similar to Value template
+std::ostream &
+operator<<( std::ostream & out, SC const & sc) { LOGGER_()
+    if ( not sc.empty() ) {
+        out << "[";                    //out.width(9);  // TODO??: neither work, only space out first element. //out << std::setw(9);  // TODO??: neither work, only space out first element.
+        // TODO??: why compile error on?: std::copy(sc.begin(), sc.end(), std::ostream_iterator< typename SC::value_type >( out, ">,<" ));
+        for( bool first{true}; auto const &i : sc) { // this works, but why not compile std::copy?
+            if(first) { out << "<" << i; first = false; }
+            else      { out << ">,<" << i; }
+        }
+        out << ">]" << " ";             // out.width(); out << std::setw(0);
+    } else
+        out << "[CONTAINTER IS EMPTY]";
+    return out;
+}
+
+namespace Detail {  // NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+//std::string source_loc();           // forward declaration
+//void stacktrace_register();
+//void crash_signals_register();
+} // End Namespace NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
---
+++ linkage_linking_bindage/main.cpp
@@ -0,0 +1,82 @@
+/** Copyright (c) Grant Rostig, grantrostig.com 2023. Distributed under the Boost Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at https://www.boost.org/LICENSE_1_0.txt)
+    NOT PRODUCTION QUALITY CODE, just shows learning/teaching example, not real programming, don't copy this style, just playing around
+    Reminder of usefull resources:
+        https://coliru.stacked-crooked.com/
+        https://godbolt.org/
+        https://cppinsights.io/
+        https://Wandbox.org
+        https://cpp.sh/
+        https://quick-bench.com/
+        https://arnemertz.github.io/online-compilers/
+    [[maybe_unused]] int * my_new_var8 = ::new (22,int);
+    Language Versions:  clan++ -std=c++2b, g++ -std=c++23 or =gnu++23
+    STD Libraries: Gcc: libstdc++; Clang: libc++; Microsoft: msvc/ms stl  // TODO??: how do we link with different libraries than the default using gcc or clang (the exact command line text)?
+    g++ -Werror -Weffc++ -Wextra -Wall -Wconversion -Wshadow -Wpedantic -Wold-style-cast -Wsign-promo -Wzero-as-null-pointer-constant -Wsuggest-override -Wnon-virtual-dtor \
+        -Wcast-align -Woverloaded-virtual -Wunused -pedantic -Wsign-conversion -Wmisleading-indentation -Wnull-dereference -Wdouble-promotion -Wformat=2 -Wimplicit-fallthrough \
+        -Wuseless-cast -Wsuggest-final-types -Wsuggest-final-methods -Wduplicated-cond -Wduplicated-branches -Wlogical-op -std=gnuc++23 \
+        main.cpp <OTHER>.cpp -o <A.OUT>
+
+    SYMBOL    MEANING // for debugging purposes
+    TODO:     the principal programmer needs todo.
+    TODO?:	  the principal programmer is not sure about something, that should be addressed.
+    TODO?:X   the X programmer is not sure about something, that should be addressed.
+    TODO??:   is a question for verbal discussion at CppMSG.com meetup meetings.
+
+    Ordering of headers as follows: re:Lakos
+    + The prototype/interface header for this implementation (ie, the .h/.hh file that corresponds to this .cpp/.cc file).
+    + Other headers from the same project, as needed.
+    + Headers from other non-standard, non-system libraries (for example, Qt, Eigen, etc).
+    + Headers from other "almost-standard" libraries (for example, Boost)
+    + Standard C++ headers (for example, iostream, functional, etc.)
+    + Standard C headers (for example, cstdint, dirent.h, etc.)
+
+    define NDEBUG if asserts are NOT to be checked.  Put in *.h file not *.CPP
+    #define NDEBUG
+    define GR_DEBUG if we/programmer is Debugging.  Put in *.h file not *.CPP
+    #define GR_DEBUG
+    #ifdef GR_DEBUG
+    #endif GR_DEBUG
+ */
+#include "global_entities.hpp"
+#include "boost_headers.hpp"
+#include "cpp_headers.hpp"
+#include "math_grostig.hpp"
+#include "ostream_joiner_gr.hpp"
+#include <boost/dynamic_bitset.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+//#include<dlib/numeric_constants.h>
+//#include <gsl/gsl>      // sudo dnf install  guidelines-support-library-devel
+//#include <bits/stdc++.h>
+#include <bit>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <csignal>
+#include <iostream>
+#include <optional>
+#include <source_location>
+#include <string>
+#include <stacktrace>
+#include <vector>
+
+using std::cin; using std::cout; using std::cerr; using std::clog; using std::endl; using std::string;  // using namespace std;
+using namespace std::string_literals;
+using namespace std::chrono_literals;
+
+namespace Detail {  // NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+} // END namespace NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN
+
+int main(int argc, char const * arv[]) {
+    string my_arv{*arv}; cout << "$$ ~~~ argc, argv:"<<argc<<","<<my_arv<<"."<<endl;
+    cin.exceptions( std::istream::failbit);
+    Detail::crash_signals_register();
+
+    LOGGER_("testing LOGGER_");
+    LOGGERX("testing LOGGER_",42);
+    //Example1::test1 ();
+
+    cout << "###" << endl;
+    return EXIT_SUCCESS;
+}
