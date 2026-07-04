af571fb28fbb8a3e0005698df928ec545403493a
Grant Rostig <grant.github@emailmeinfo.com>
2026-07-03 21:28:26 -0500

    rename and array

--- std_latch_barrier/main.cpp
+++ std_latch_barrier/main.cpp
@@ -1,3 +1,4 @@
+#include <array>
 #include <barrier>
 #include <functional>
 #include <iostream>
@@ -7,20 +8,22 @@
 using std::cout, std::endl, std::cin;
 int f(){return 42;};
 struct Job {
-    const std::string name{};
-    std::string product{"not worked"};
-    std::thread action{};
+    const std::string   name{};
+    std::string         product{"not worked"};
+    std::thread         action{};
 };
 int main() {
-    Job jobs[]{{"Annika"}, {"Buru"}, {"Chuck"}};
-    std::latch work_done_not_cleaned_yet_wait_latch_multiple{std::size(jobs)};
-    std::latch start_clean_up_post_work_wait_latch_single{1};
+    std::array<Job, 3>  jobs{{{"Annika"}, {"Buru"}, {"Chuck"}}};
+    std::latch          multLatch_wait_for_work_done_prior_to_clean{std::ssize(jobs)};
+    // std::latch          w2rk_done_not_cleaned_yet_wait_latch_multiple{std::size(jobs)};
+	// std::latch          w3rk_done_not_cleaned_yet_wait_latch_multiple{ jobs.ssize() }; // TODO??: ssize() is C++20, not C++17
+    std::latch          uniLatch_wait_for_worked_to_start_clean_up{true};
 
-    auto work_and_clean_eventually = [&](Job& my_job) {
-        my_job.product = my_job.name + " worked";
-        work_done_not_cleaned_yet_wait_latch_multiple.count_down();
-        start_clean_up_post_work_wait_latch_single.wait();
-        my_job.product = my_job.name + " cleaned";
+    auto work_and_clean_eventually = [&multLatch_wait_for_work_done_prior_to_clean, &uniLatch_wait_for_worked_to_start_clean_up](Job& my_job) {
+        my_job.product=my_job.name + " worked";
+        multLatch_wait_for_work_done_prior_to_clean.count_down();
+        uniLatch_wait_for_worked_to_start_clean_up.wait();
+        my_job.product=my_job.product + " and cleaned.";
     };
 
     std::cout << "Work is starting... ";
@@ -28,12 +31,12 @@ int main() {
         job.action = std::thread{work_and_clean_eventually, std::ref(job)};
         // job.action = std::thread{work, job};  // TODO??: invocable?
 
-    work_done_not_cleaned_yet_wait_latch_multiple.wait(); std::cout << "done:\n";
+    multLatch_wait_for_work_done_prior_to_clean.wait(); std::cout << "done:\n";
     for (auto const& job : jobs)
         std::cout << job.product << '\n';
 
     std::cout << "Workers are cleaning up... ";
-    start_clean_up_post_work_wait_latch_single.count_down();
+    uniLatch_wait_for_worked_to_start_clean_up.count_down();
     for (auto& job : jobs)
         job.action.join();
 
